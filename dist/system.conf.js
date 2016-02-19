a576fdad3b9668f0d604d4c647d495832f536b4d
Joppe Aarts <joppe@zicht.nl>
2016-02-19 12:52:32 +0100

    jshint

--- dist/system.conf.js
+++ dist/system.conf.js
@@ -2,13 +2,14 @@
 
 var systemLocate = System.locate;
 
-System.locate = function(load) {
+System.locate = function (load) {
     var System = this;
 
     return Promise.resolve(systemLocate.call(this, load)).then(function(address) {
         return address + System.cacheBust;
     });
-}
+};
+
 System.cacheBust = '?bust=' + Date.now();
 
 System.config({
