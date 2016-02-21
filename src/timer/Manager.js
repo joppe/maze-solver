16b0d7d4e52f4b070b57e3c199502fbbb302b50e
Joppe Aarts <joppe@apestaartje.info>
2016-02-21 17:52:34 +0100

    Timer manager is object literal and getIterator removed, the literal is iteratable

--- src/timer/Manager.js
+++ src/timer/Manager.js
@@ -2,87 +2,76 @@ import {Timer} from './Timer.js';
 
 let timers = {};
 
-/**
- * @class Manager
- */
-export class Manager {
+export var Manager = {
     /**
      * @param {string} id
      * @returns {Timer}
      */
-    static getTimer(id) {
+    getTimer(id) {
         if (undefined === timers[id]) {
             timers[id] = new Timer();
         }
 
         return timers[id];
-    }
+    },
 
     /**
      * @param {string} id
      * @returns {Timer}
      */
-    static start(id) {
+    start(id) {
         let timer = Manager.getTimer(id);
 
         timer.start();
 
         return timer;
-    }
+    },
+
     /**
      * @param {string} id
      * @returns {Timer}
      */
-    static end(id) {
+    end(id) {
         let timer = Manager.getTimer(id);
 
         timer.stop();
 
         return timer;
-    }
+    },
 
     /**
      * @param {string} id
      * @returns {number}
      */
-    static duration(id) {
+    duration(id) {
         return getTimer(id).getDuration();
-    }
+    },
 
-    /**
-     * @returns {Iterator}
-     */
-    static getIterator() {
+    [Symbol.iterator]() {
         let current = null,
             ids = Object.keys(timers);
 
         return {
-            [Symbol.iterator]() {
-                let step = 0;
+            next() {
+                let value;
+
+                if (null === current) {
+                    current = ids[0];
+                    value = current;
+                } else {
+                    let index = ids.indexOf(current);
+
+                    if (-1 !== index && ids.length > (index + 1)) {
+                        current = ids[index + 1];
+                        value = current;
+                    }
+                }
 
                 return {
-                    next() {
-                        let value;
-
-                        if (null === current) {
-                            current = ids[0];
-                            value = current;
-                        } else {
-                            let index = ids.indexOf(current);
-
-                            if (-1 !== index && ids.length > (index + 1)) {
-                                current = ids[index + 1];
-                                value = current;
-                            }
-                        }
-
-                        return {
-                            value,
-                            done: undefined === value
-                        };
-                    }
+                    value,
+                    done: undefined === value
                 };
             }
         };
     }
-}
+};
